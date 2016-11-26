0414357a86703ea6a7cdd7c5f90a4b2e6f58c032
Andrew Bennett <potatosaladx@gmail.com>
2016-11-26 16:09:34 -0700

    Deactivate wrist while in claw mode.

--- src/wrist.c
+++ src/wrist.c
@@ -4,7 +4,11 @@ static const motor_t wrist = {6, false};
 
 void wrist_tick(joystick_t *joy) {
   // for Motors in Port 7
-   motorReflect(&wrist, joy->ch2.value * robot.reflected);
+  if (!joy->claw) {
+    motorReflect(&wrist, joy->ch2.value * robot.reflected);
+  } else {
+    motorReflect(&wrist, 0);
+  }
   // if(joystickGetDigital(1, 5, JOY_UP) == 1){
   //   motorReflect(&wrist, 127);
   // }
