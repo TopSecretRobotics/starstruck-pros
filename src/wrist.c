c5d4a10c64eb31b22d188051b720a5013dcf6ff5
SaxophoneTenor <jakeolaveson@gmail.com>
2016-11-26 15:59:04 -0700

    just saved wrist

---
+++ src/wrist.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+static const motor_t wrist = {6, false};
+
+void wrist_tick(joystick_t *joy) {
+  // for Motors in Port 7
+   motorReflect(&wrist, joy->ch2.value * robot.reflected);
+
+  //  
+  // if(joystickGetDigital(1, 5, JOY_UP) == 1){
+  //   motorReflect(&wrist, 127);
+  // }
+  //
+  // if(joystickGetDigital(1, 5, JOY_DOWN) == 1){
+  //   motorReflect(&wrist, -127);
+  // }
+  //
+  // if((joystickGetDigital(1, 5, JOY_UP) == 0) && (joystickGetDigital(1, 5, JOY_DOWN) == 0)){
+  //   motorReflect(&wrist, 0);
+  // }
+}
